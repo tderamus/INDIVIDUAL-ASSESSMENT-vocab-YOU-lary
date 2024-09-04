0eccd73996dad4ed5d5246ce89383d14f4c7c29e
tderamus <deramust@gmail.com>
2024-09-03 19:14:28 -0500

    added functions to main.js, startApp.js, and viewDirector to initiate the app login with firebase

--- README.md
+++ README.md
@@ -57,7 +57,7 @@ Here is how we add our styles using webpack:
 import '../styles/main.scss';
 
 const init = () => {
-  document.querySelector('#app').innerHTML = '<h1>HELLO! You are up and running!</h1>');
+  document.querySelector('#app').innerHTML = '<h1>HELLO! You are up and running!</h1>';
   console.log('YOU ARE UP AND RUNNING!');
 };
 
--- utils/viewDirector.js
+++ utils/viewDirector.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import loginButton from '../components/loginButton';
-import logoutButton from '../components/logoutButton';
 import client from './client';
+import startApp from './startApp';
 
 const ViewDirectorBasedOnUserAuthStatus = () => {
   firebase.initializeApp(client);
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // person is logged in do something...
-      logoutButton();
+      console.warn(user);
+      startApp(user);
     } else {
       // person is NOT logged in
       loginButton();
