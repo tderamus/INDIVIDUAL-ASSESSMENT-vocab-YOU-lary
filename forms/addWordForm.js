e38c2a6f5544a1320cb7f83f47c6a20253af03b4
tderamus <deramust@gmail.com>
2024-09-11 18:02:58 -0500

    added filter API function to filter network words on the DOM

--- forms/addWordForm.js
+++ forms/addWordForm.js
@@ -1,5 +1,6 @@
 import clearDom from '../utils/clearDom';
 import renderToDOM from '../utils/renderToDom';
+import selectCategory from './selectCategory';
 
 const addWordForm = (obj = {}) => {
   clearDom();
@@ -10,16 +11,16 @@ const addWordForm = (obj = {}) => {
     <div class="mb-3">
     <input type="text" class="form-control" id="definition" rows="3" placeholder="add definition" value="${obj.definition || ''}"></input>
     </div>
-    <div class="mb-3">
-    <input type="text" class="form-control" id="category" placeholder="add category" value="${obj.category || ''}"></div>
     <div class="form-check">
-    <div class="form-group" id="select-category"></div>
-    </div><input type="checkbox" id="network" class="form-check-input" ${obj.network ? 'checked' : ''}>
+    <div class="form-group" id="select-category">Here</div>
+    </div>
+    <input type="checkbox" id="network" class="form-check-input" ${obj.network ? 'checked' : ''}>Network Type
     </div>
     <button type="submit" class="btn btn-primary mt-3" id="submit-words">Submit Word</button>
   </form>`;
 
   renderToDOM('#main-container', domString);
+  selectCategory(`${obj.category || ''}`);
 };
 
 export default addWordForm;
